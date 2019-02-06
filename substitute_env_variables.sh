9e67a8d043532dd179d8a78dc01d18085f220256
DanielHabenicht <daniel-habenicht@outlook.de>
2019-02-07 00:18:04 +0100

    Update substitute_env_variables.sh
    Add some more explanations

--- substitute_env_variables.sh
+++ substitute_env_variables.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# The first parameter should be the path to the file which should be substituted
+# The first parameter is the path to the file which should be substituted
 if [[ -z $1 ]]; then
     echo 'ERROR: No target file given.'
     exit 1
 fi
 
-# Use the included envsubst command (not available in every docker container) to substitute the variables
+# The included envsubst command (not available in every docker container) will substitute the variables for us.
+# They should have the format ${TEST_ENV} or $TEST_ENV
+# For more information look up the command here: https://www.gnu.org/software/gettext/manual/html_node/envsubst-Invocation.html
 envsubst '\$TEST_ENV \$OTHER_ENV' < $1 > $1
 
-# Execute all other paramters
+# Execute all other commands with paramters
 exec "${@:2}"
